97bb63604e39ce16c6c5e7fd9091a8a9023cd3ce
Sanja Wetzel <sanjwich@gmail.com>
2020-12-21 13:24:22 +0100

    first commit, playing around with components

---
+++ part1/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
